b51efa0bce5fc5454b05b8f8cdfbb1b9b90282bf
Hassan <hhalnajj@uci.edu>
2022-08-18 16:27:48 -0700

    Fixed shrink/growing of divs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-window.addEventListener('resize', () => console.log("resized!"));
 
 document.addEventListener('keydown', (e) => {
     if (e.key == "w" || e.key == 'ArrowUp') {
@@ -14,3 +13,22 @@ document.addEventListener('keydown', (e) => {
         console.log("right");
     }
 })
+
+
+function createGrid() {
+    var content = document.getElementById('content');
+    rows = Math.floor(screen.width / 20);
+    columns = Math.floor(screen.height / 20);
+}
+
+let rows = 0;
+let columns = 0;
+createGrid();
+
+window.addEventListener('resize', () => 
+{
+    createGrid();
+    console.log("rows -> ", rows);
+    console.log("columns -> ", columns);
+
+});
