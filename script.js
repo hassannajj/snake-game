1e76268f948612860cf8c07bc438cb18b2167f0e
Hassan <hhalnajj@uci.edu>
2022-08-19 08:48:41 -0700

    Fixed grid

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Advanced Settings
+
+
+
 
 document.addEventListener('keydown', (e) => {
     if (e.key == "w" || e.key == 'ArrowUp') {
@@ -14,15 +18,42 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
+function reset() {
+    let squares = content.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+}
+
 
 function createGrid() {
-    var content = document.getElementById('content');
-    rows = Math.floor(screen.width / 20);
-    columns = Math.floor(screen.height / 20);
+    // Have to reset back to the origin using resetGame()
+    reset();
+
+    columns = Math.floor(content.offsetWidth/12);
+    rows = Math.floor(content.offsetHeight/12);
+    console.log(rows, columns);
+
+    content.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    content.style.gridTemplateRows = `repeat(${rows}, 1fr)`;;
+
+    let amount = columns * rows;
+    for (let i=0; i<amount; i++) {
+        let square = document.createElement('div');
+        square.className = 'square';
+        content.appendChild(square);
+    }
+    
+}
+
+function setContentSize() {
+    //content.style.width = '500px';
+    //content.style.height = '500px';
 }
 
 let rows = 0;
 let columns = 0;
+let content = document.getElementById('content');
+
+setContentSize();
 createGrid();
 
 window.addEventListener('resize', () => 
