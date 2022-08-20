239808aa20b8ffc283b142de5716aa9bed6068ce
Hassan <hhalnajj@uci.edu>
2022-08-20 10:57:09 -0700

    implemented food

--- script.js
+++ script.js
@@ -19,47 +19,51 @@ document.addEventListener('keydown', (e) => {
 })
 
 function reset() {
+    // deletes all the divs
     let squares = content.querySelectorAll('div');
     squares.forEach((div) => div.remove());
 }
 
+function placeFood() {
+    // Creates and places food in a random square on the grid
+    let foodX = Math.floor(Math.random() * columns);
+    let foodY = Math.floor(Math.random() * rows);
+
+    let foodDiv = document.getElementById(`${foodX}-${foodY}`);
+    foodDiv.style.backgroundColor = 'red';
+}
 
 function createGrid() {
-    // Have to reset back to the origin using resetGame()
-    reset();
+    reset();  // change the name of this function maybe
 
+    // Put this 12 on the advanced settings js / json / txtfile
     columns = Math.floor(content.offsetWidth/12);
     rows = Math.floor(content.offsetHeight/12);
-    console.log(rows, columns);
 
     content.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     content.style.gridTemplateRows = `repeat(${rows}, 1fr)`;;
 
-    let amount = columns * rows;
-    for (let i=0; i<amount; i++) {
-        let square = document.createElement('div');
-        square.className = 'square';
-        content.appendChild(square);
+    for (let i=0; i<rows; i++) {
+        for (let j=0; j<columns; j++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+            square.id = `${j}-${i}`;
+            content.appendChild(square);
+        }
     }
-    
-}
-
-function setContentSize() {
-    //content.style.width = '500px';
-    //content.style.height = '500px';
 }
 
 let rows = 0;
 let columns = 0;
 let content = document.getElementById('content');
 
-setContentSize();
 createGrid();
+placeFood();
 
 window.addEventListener('resize', () => 
 {
     createGrid();
-    console.log("rows -> ", rows);
-    console.log("columns -> ", columns);
+    placeFood();
+
 
 });
